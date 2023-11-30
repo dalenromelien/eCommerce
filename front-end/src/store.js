341107cbeeada6724dc5bc4da5d18c79fc76fb40
Dalen Romelien <dromelie@nyit.edu>
2023-11-29 21:45:50 -0500

    completed chapter 6

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSLiceReducer from "./slices/cartSlice.js";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSLiceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
