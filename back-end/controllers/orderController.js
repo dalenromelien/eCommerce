2c64fcaac9070057a4cebbeb2ba8768d5acdb86a
dromelie <dromelie@nyit.edu>
2024-04-02 09:57:54 -0400

    getOrders and getMyOrders

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -45,7 +45,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route GET /api/orders/myorders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({}).populate('user', 'id name');
+  const orders = await Order.find({ user: req.user._id });
   res.status(200).json(orders);
 });
 
@@ -101,7 +101,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 // @route GET /api/orders
 // @access Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send("get all orders");
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.status(200).json(orders);
 });
 
 export {
